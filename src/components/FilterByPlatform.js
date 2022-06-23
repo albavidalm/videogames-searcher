07abb8c30daf2ce38f9ce233beb5b6b377c9aadd
albavidalm <albavidalm@gmail.com>
2022-06-23 13:29:30 +0200

    Add new platforms

--- src/components/FilterByPlatform.js
+++ src/components/FilterByPlatform.js
@@ -20,31 +20,45 @@ const FilterByPlatform = (props) => {
       >
         <option value="all">All</option>
         <option value="Android">Android</option>
+        <option value="Jaguar">Atari Jaguar</option>
+        <option value="Atari ST">Atari ST</option>
+        <option value="Commodore / Amiga">Commodore / Amiga</option>
+        <option value="Game Boy">Game Boy</option>
+        <option value="Game Boy Advance">Game Boy Advance</option>
+        <option value="Game Boy Color">Game Boy Color</option>
         <option value="iOS">iOS</option>
         <option value="Linux">Linux</option>
+        <option value="Classic Macintosh">Macintosh Classic</option>
         <option value="macOS">macOS</option>
-        <option value="NES">Nes</option>
-        <option value="SNES">SNes</option>
-        <option value="Nintendo DS">Nintendo DS</option>
         <option value="Nintendo 3DS">Nintendo 3DS</option>
         <option value="Nintendo 64">Nintendo 64</option>
-        <option value="Nintendo GameCube">Nintendo GameCube</option>
+        <option value="Nintendo DS">Nintendo DS</option>
+        <option value="NES">Nes</option>
+        <option value="SNES">SNes</option>
+        <option value="GameCube">Nintendo GameCube</option>
         <option value="Nintendo Switch">Nintendo Switch</option>
+        <option value="3DO">Panasonic 3DO</option>
         <option value="PC">PC</option>
+        <option value="PlayStation">PlayStation</option>
         <option value="PlayStation 2">PlayStation 2</option>
         <option value="PlayStation 3">PlayStation 3</option>
         <option value="PlayStation 4">PlayStation 4</option>
         <option value="PlayStation 5">PlayStation 5</option>
         <option value="PSP">PSP</option>
         <option value="PS Vita">PS Vita</option>
+        <option value="SEGA CD">Sega CD</option>
+        <option value="Dreamcast">Sega Dreamcast</option>
+        <option value="Game Gear">Sega Game Gear</option>
         <option value="Genesis">Sega Genesis</option>
+        <option value="SEGA Master System">Sega Master System</option>
+        <option value="SEGA Saturn">Sega Saturn</option>
+        <option value="Xbox">Xbox</option>
+        <option value="Xbox 360">Xbox 360</option>
+        <option value="Xbox One">Xbox One</option>
+        <option value="Xbox Series S/X">Xbox Series S/X</option>
         <option value="Web">Web</option>
         <option value="Wii">Wii</option>
         <option value="Wii U">Wii U</option>
-        <option value="Xbox 360">Xbox 360</option>
-        <option value="Xbox">Xbox</option>
-        <option value="Xbox One">Xbox One</option>
-        <option value="Xbox Series S/X">Xbox Series S/X</option>
       </select>
     </>
   );
--- src/services/getIconPlatform.js
+++ src/services/getIconPlatform.js
@@ -1,55 +1,83 @@
 import android from "../assets/images/android.svg";
+import atarijaguar from "../assets/images/atarijaguar.svg";
+import atarist from "../assets/images/atarist.svg";
+import commodore from "../assets/images/commodore.svg";
+import gameboy from "../assets/images/gameboy.svg";
+import gameboyadv from "../assets/images/gameboyadv.svg";
+import gameboycolor from "../assets/images/gameboycolor.svg";
 import ios from "../assets/images/ios.svg";
 import linux from "../assets/images/linux.svg";
+import macintosh from "../assets/images/macintosh.svg";
 import macos from "../assets/images/macos.svg";
+import n3ds from "../assets/images/n3ds.svg";
+import n64 from "../assets/images/n64.svg";
+import nds from "../assets/images/nds.svg";
+import nes from "../assets/images/nes.svg";
+import ngamecube from "../assets/images/ngamecube.svg";
+import nsnes from "../assets/images/nsnes.svg";
+import nswitch from "../assets/images/nswitch.svg";
+import p3do from "../assets/images/p3do.svg";
 import pc from "../assets/images/pc.svg";
+import playstation from "../assets/images/playstation.svg";
 import ps2 from "../assets/images/ps2.svg";
 import ps3 from "../assets/images/ps3.svg";
 import ps4 from "../assets/images/ps4.svg";
 import ps5 from "../assets/images/ps5.svg";
 import psp from "../assets/images/psp.svg";
 import psvita from "../assets/images/psvita.svg";
-import n3ds from "../assets/images/n3ds.svg";
-import n64 from "../assets/images/n64.svg";
-import nds from "../assets/images/nds.svg";
-import nes from "../assets/images/nes.svg";
-import ngamecube from "../assets/images/ngamecube.svg";
-import snes from "../assets/images/snes.svg";
-import nswitch from "../assets/images/nswitch.svg";
+import scd from "../assets/images/scd.svg";
+import sdreamcast from "../assets/images/sdreamcast.svg";
+import sgamegear from "../assets/images/sgamegear.svg";
 import sgenesis from "../assets/images/sgenesis.svg";
-import xone from "../assets/images/xone.svg";
-import xseries from "../assets/images/xseries.svg";
-import xbox from "../assets/images/xbox.svg";
-import x360 from "../assets/images/x360.svg";
+import smastersystem from "../assets/images/smastersystem.svg";
+import ssaturn from "../assets/images/ssaturn.svg";
 import web from "../assets/images/web.svg";
 import wii from "../assets/images/wii.svg";
 import wiiu from "../assets/images/wii-u.svg";
+import x360 from "../assets/images/x360.svg";
+import xbox from "../assets/images/xbox.svg";
+import xone from "../assets/images/xone.svg";
+import xseries from "../assets/images/xseries.svg";
 
 const getIconPlatform = (platforms) => {
   return (
     (platforms === "Android" && android) ||
+    (platforms === "Jaguar" && atarijaguar) ||
+    (platforms === "Atari ST" && atarist) ||
+    (platforms === "Commodore / Amiga" && commodore) ||
+    (platforms === "Game Boy" && gameboy) ||
+    (platforms === "Game Boy Advance" && gameboyadv) ||
+    (platforms === "Game Boy Color" && gameboycolor) ||
     (platforms === "iOS" && ios) ||
     (platforms === "Linux" && linux) ||
+    (platforms === "Classic Macintosh" && macintosh) ||
     (platforms === "macOS" && macos) ||
+    (platforms === "Nintendo 3DS" && n3ds) ||
+    (platforms === "Nintendo 64" && n64) ||
+    (platforms === "Nintendo DS" && nds) ||
+    (platforms === "NES" && nes) ||
+    (platforms === "SNES" && nsnes) ||
+    (platforms === "GameCube" && ngamecube) ||
+    (platforms === "Nintendo Switch" && nswitch) ||
+    (platforms === "3DO" && p3do) ||
     (platforms === "PC" && pc) ||
+    (platforms === "PlayStation" && playstation) ||
     (platforms === "PlayStation 2" && ps2) ||
     (platforms === "PlayStation 3" && ps3) ||
     (platforms === "PlayStation 4" && ps4) ||
     (platforms === "PlayStation 5" && ps5) ||
     (platforms === "PSP" && psp) ||
     (platforms === "PS Vita" && psvita) ||
-    (platforms === "Nintendo 3DS" && n3ds) ||
-    (platforms === "Nintendo 64" && n64) ||
-    (platforms === "Nintendo DS" && nds) ||
-    (platforms === "NES" && nes) ||
-    (platforms === "GameCube" && ngamecube) ||
-    (platforms === "SNES" && snes) ||
-    (platforms === "Nintendo Switch" && nswitch) ||
+    (platforms === "SEGA CD" && scd) ||
+    (platforms === "Dreamcast" && sdreamcast) ||
+    (platforms === "Game Gear" && sgamegear) ||
     (platforms === "Genesis" && sgenesis) ||
-    (platforms === "Xbox One" && xone) ||
-    (platforms === "Xbox Series S/X" && xseries) ||
+    (platforms === "SEGA Master System" && smastersystem) ||
+    (platforms === "SEGA Saturn" && ssaturn) ||
     (platforms === "Xbox" && xbox) ||
     (platforms === "Xbox 360" && x360) ||
+    (platforms === "Xbox One" && xone) ||
+    (platforms === "Xbox Series S/X" && xseries) ||
     (platforms === "Web" && web) ||
     (platforms === "Wii" && wii) ||
     (platforms === "Wii U" && wiiu)
